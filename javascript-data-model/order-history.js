152a42fd84ab926ef27693547ed3eec71f46a18f
Kenny Park <kennyparc@gmail.com>
2021-08-24 13:00:22 -0700

    fixed arrays and objects system for the orders and items

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,30 +3,42 @@ var orderHistory = [
     orderNumber: '114-3941689-8772232',
     orderDate: 'August 4, 2020',
     totalCost: '$34.00',
-    title: 'JavaScript for impatient programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    itemCost: '$31.55',
     deliveredDate: 'August 8, 2020',
-    returnWindowClosed: 'September 7, 2020'
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemCost: '$31.55',
+        returnWindowClosed: 'September 7, 2020'
+      }
+    ]
   },
   {
     orderNumber: '113-9984268-1280257',
     orderDate: 'July 19, 2020',
     totalCost: '$44.53',
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    itemCost: '$41.33',
     deliveredDate: 'July 20, 2020',
-    returnWindowClosed: 'August 19, 2020'
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemCost: '$41.33',
+        returnWindowClosed: 'August 19, 2020'
+      }
+    ]
   },
   {
     orderNumber: '114-2875557-9059409',
     orderDate: 'July 4, 2020',
     totalCost: '$17.22',
-    title: 'Gamecube Controller Adapter',
-    itemCost: '$15.98',
     deliveredDate: 'July 7, 2020',
-    returnWindowClosed: 'August 5, 2020'
+    items: [
+      {
+        title: 'Gamecube Controller Adapter',
+        itemCost: '$15.98',
+        returnWindowClosed: 'August 5, 2020'
+      }
+    ]
   },
   {
     orderNumber: '113-2883177-2648248',
@@ -50,6 +62,6 @@ var orderHistory = [
 ];
 
 console.log('value of orderHistory:', orderHistory);
-console.log('value of fourth order\'s second item array:', orderHistory[3].items[1]);
+console.log('value of first order\'s item info:', orderHistory[0].items);
 console.log('value of fourth order\'s second item\'s itemCost property:', orderHistory[3].items[1].itemCost);
 console.log('value of fourth order\'s first item\'s returnWindowClosed property:', orderHistory[3].items[0].returnWindowClosed);
