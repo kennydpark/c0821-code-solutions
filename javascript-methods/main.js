05b701a803fb5df5dc447ebc956d95d6387e1ddf
Kenny Park <kennyparc@gmail.com>
2021-08-25 11:02:42 -0700

    Completed javascript-methods exercise.

---
+++ javascript-methods/main.js
@@ -0,0 +1,56 @@
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var maximumValue = Math.max(numberOne, numberTwo, numberThree);
+console.log('value of maximumValue:', maximumValue);
+var heroes = ['Spiderman', 'Batman', 'Ironman'];
+var randomNumber = Math.random();
+randomNumber *= heroes.length;
+var randomIndex = Math.floor(randomNumber);
+console.log('value of randomIndex:', randomIndex);
+var randomHero = heroes[randomIndex];
+console.log('value of randomHero:', randomHero);
+
+var library = [
+  {
+    title: 'Harry Potter',
+    author: 'J. K. Rowling'
+  },
+  {
+    title: 'The Hobbit',
+    author: 'J. R. R. Tolkien'
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J. D. Salinger'
+  }
+];
+
+var lastBook = library.pop();
+console.log('value of lastBook:', lastBook);
+var firstBook = library.shift();
+console.log('value of firstBook:', firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+
+console.log('value of library:', library);
+
+var fullName = 'Kenny Park';
+var firstAndLastName = fullName.split(' ');
+console.log('value of firstAndLastName:', firstAndLastName);
+
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('value of sayMyName:', sayMyName);
