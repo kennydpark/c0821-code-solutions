5bdd05a59d0cc149d95bb7bfb18ecad8642f1132
Kenny Park <kennyparc@gmail.com>
2021-08-31 16:26:11 -0700

    used booleans stored in variables  instead of DOM element classes in function.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,31 @@
 var button = document.querySelector('.button');
 var circle = document.querySelector('.circle');
 var body = document.querySelector('.body');
+var isOn = (button.className === 'button button-on');
 button.addEventListener('click', click);
 
 function click(event) {
-  if (button.className === 'button button-off') {
+  if (isOn === false) {
     button.className = 'button button-on';
     circle.className = 'circle circle-on';
     body.className = 'body body-on';
-  } else if (button.className === 'button button-on') {
+    isOn = true;
+  } else if (isOn === true) {
     button.className = 'button button-off';
     circle.className = 'circle circle-off';
     body.className = 'body body-off';
+    isOn = false;
   }
 }
+
+// function click(event) {
+//   if (button.className === 'button button-off') {
+//     button.className = 'button button-on';
+//     circle.className = 'circle circle-on';
+//     body.className = 'body body-on';
+//   } else if (button.className === 'button button-on') {
+//     button.className = 'button button-off';
+//     circle.className = 'circle circle-off';
+//     body.className = 'body body-off';
+//   }
+// }
