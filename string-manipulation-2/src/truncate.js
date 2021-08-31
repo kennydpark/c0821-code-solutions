b27dc3539fda96218c2cdc69c01d4aac644c46b6
Kenny Park <kennyparc@gmail.com>
2021-08-30 22:52:48 -0700

    fixed pseudocode for truncate.js after changes.

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -18,11 +18,12 @@ function truncate(length, string) {
 // input: length(number), string
 // output: string
 // create storage for output, which is empty string
+// if string strictly equals empty string,
+//    return '...'
 // look at each char one at a time, if var i is less than the value of length param
 // if length is greater than or equal to the length of the string,
 //    add the value of string at i to output
 //    otherwise, if length is greater than the length of the string,
 //        update length to equal the length of the string.
-//        look at each char one at a time, if the var i is less than the value of current length
-//            add the value of string at i to output
+//        add the value of string at i to output, then assign result to output
 // return output concatenated with '...'
