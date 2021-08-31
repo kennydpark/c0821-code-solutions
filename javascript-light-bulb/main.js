593953069977480f5159d50a703566f0a8e27005
Kenny Park <kennyparc@gmail.com>
2021-08-31 16:26:59 -0700

    removed pseudocode

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -17,15 +17,3 @@ function click(event) {
     isOn = false;
   }
 }
-
-// function click(event) {
-//   if (button.className === 'button button-off') {
-//     button.className = 'button button-on';
-//     circle.className = 'circle circle-on';
-//     body.className = 'body body-on';
-//   } else if (button.className === 'button button-on') {
-//     button.className = 'button button-off';
-//     circle.className = 'circle circle-off';
-//     body.className = 'body body-off';
-//   }
-// }
