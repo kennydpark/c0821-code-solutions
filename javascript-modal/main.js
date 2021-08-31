9d9a2389c30b2cc20509725e53eca6d8d036f4d5
Kenny Park <kennyparc@gmail.com>
2021-08-31 16:38:10 -0700

    removed non-functioning variable and conditional.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,24 +1,21 @@
 var openModal = document.querySelector('.open-modal');
 var modalFull = document.querySelector('.modal-full');
 var modalWindow = document.querySelector('.column');
-var isOff = (modalFull.className === 'modal-full off');
-var button = document.querySelector('.modal-no');
+var modalNo = document.querySelector('.modal-no');
 var text = document.querySelector('.text-no');
 
 openModal.addEventListener('click', function (event) {
-  if (isOff) {
-    modalFull.className = 'modal-full on';
-    button.className = 'modal-no button-on';
-    text.className = 'text-no white button-on';
-    modalWindow.className = 'column modal-window-on';
-  }
+  modalFull.className = 'modal-full on';
+  modalNo.className = 'modal-no button-on';
+  text.className = 'text-no white button-on';
+  modalWindow.className = 'column modal-window-on';
 });
 
-button.addEventListener('click', modalClick);
+modalNo.addEventListener('click', modalClick);
 
 function modalClick(event) {
   modalFull.className = 'modal-full off';
-  button.className = 'modal-no button-off';
+  modalNo.className = 'modal-no button-off';
   text.className = 'text-no white button-off';
   modalWindow.className = 'column modal-window-off';
 }
