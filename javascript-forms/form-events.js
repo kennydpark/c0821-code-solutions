07cdd02e3c7173d5485a7587eb2f88d319b8470b
Kenny Park <kennyparc@gmail.com>
2021-09-01 16:01:39 -0700

    moved form-values code to correct js file

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -27,18 +27,3 @@ userEmail.addEventListener('input', handleInput);
 textArea.addEventListener('focus', handleFocus);
 textArea.addEventListener('blur', handleBlur);
 textArea.addEventListener('input', handleInput);
-
-var contactForm = document.querySelector('form');
-
-contactForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var output = {};
-  var nameValue = contactForm.elements.name.value;
-  var emailValue = contactForm.elements.email.value;
-  var textAreaValue = contactForm.elements.message.value;
-  output.name = nameValue;
-  output.email = emailValue;
-  output.message = textAreaValue;
-  console.log(output);
-  document.querySelector('#contact-form').reset();
-});
