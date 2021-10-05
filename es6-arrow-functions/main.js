bab6a2a37c6d5e466941add5b26b99871fc20ca5
Kenny Park <kennyparc@gmail.com>
2021-10-05 18:55:41 +0000

    Changed all the setTimeout functions to arrow functions to disregard the use of reassigning the 'this' objects that were shadowed.

--- es6-arrow-functions/main.js
+++ es6-arrow-functions/main.js
@@ -5,35 +5,34 @@ const $jokeForm = document.querySelector('#joke-form');
 const jokester = {
   tellJoke: function (setup, punchline) {
     $jokeForm.classList.add('d-none');
-    const self = this;
-    const $introStatement = self.renderJokePhrase('Hey Flash...');
+    const $introStatement = this.renderJokePhrase('Hey Flash...');
     this.appendJokePhrase($introStatement);
-    setTimeout(function () {
-      const $jokeSetup = self.renderJokePhrase(setup);
-      self.appendJokePhrase($jokeSetup);
-      setTimeout(function () {
-        const $jokePunchline = self.renderJokePhrase(punchline);
-        self.appendJokePhrase($jokePunchline);
+    setTimeout(() => {
+      const $jokeSetup = this.renderJokePhrase(setup);
+      this.appendJokePhrase($jokeSetup);
+      setTimeout(() => {
+        const $jokePunchline = this.renderJokePhrase(punchline);
+        this.appendJokePhrase($jokePunchline);
         flash.laugh();
       }, 2000);
     }, 2000);
   },
-  renderJokePhrase: phrase => {
+  renderJokePhrase: function (phrase) {
     const $phrase = document.createElement('h4');
     $phrase.textContent = phrase;
     $phrase.className = 'fade-in text-center';
     return $phrase;
   },
-  appendJokePhrase: $phrase => {
+  appendJokePhrase: function ($phrase) {
     $jokeContainer.append($phrase);
   }
 };
 
 const flash = {
   laughingUrl: 'images/flash-laugh.gif',
-  laugh: () => {
+  laugh: function () {
     setTimeout(() => {
-      $flashImage.setAttribute('src', flash.laughingUrl);
+      $flashImage.setAttribute('src', this.laughingUrl);
     }, 3000);
   }
 };
