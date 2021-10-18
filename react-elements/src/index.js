14ed7b8f3cbcf3a4cccc92424d961c47985904fb
Kenny Park <kennyparc@gmail.com>
2021-10-18 23:44:55 +0000

    Completed react-elements exercise.

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'Hello, React!');
+ReactDOM.render(title, document.getElementById('root'));
