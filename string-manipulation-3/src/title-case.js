42dfed167e7a0ec1d9382b7b8ca46157d891fd17
Kenny Park <kennyparc@gmail.com>
2021-10-24 02:49:40 +0000

    Set a condition that checked if a word was hyphenated, then converted it to the correct case style - instead of hard-coding the 2 examples with hyphenated words.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -9,10 +9,9 @@ function titleCase(title) {
       words[i] = 'JavaScript:';
     } else if (words[i] === 'api') {
       words[i] = 'API';
-    } else if (words[i] === 'in-depth') {
-      words[i] = 'In-Depth';
-    } else if (words[i] === 'self-taught') {
-      words[i] = 'Self-Taught';
+    } else if (/[-]/.test(words[i]) === true) {
+      var separated = words[i].split('-');
+      words[i] = separated[0][0].toUpperCase() + separated[0].substr(1) + '-' + separated[1][0].toUpperCase() + separated[1].substr(1);
     }
     if ((minor.indexOf(words[i]) === -1)) {
       words[i] = words[i][0].toUpperCase() + words[i].substr(1);
@@ -35,15 +34,18 @@ function titleCase(title) {
 // convert the string into an array, where each element is a word. Assign result to 'words' variable
 // create an array of all the minor words
 // loop through the words array
-//    if the current equals 'javascript', 'javascript:', 'api', 'in-depth', or 'self-taught',
+//    if the current word equals 'javascript', 'javascript:', 'api',
 //        change the current word to the correct case style
-// if the current word does not equal a minor word,
-//    capitalize the first letter and combine with the rest of the letters, then assign the result to the current word
-// loop through the new words array
-//    convert the first word's first letter to upper case
-//    if the current word equals 'JavaScript:',
-//        change the next word to the correct case style
-//    othewrise, if the word equals 'Programmer:',
-//        change the next word to the correct case style
-// convert the array of words into a string
-// return the string
+//    if the current word contains a hyphen,
+//        convert the word into an array, separated by the hyphen - into 2 words
+//        concatenate the first word's first letter uppercased, rest of the letters of the first word, a hyphen, the second word's first letter uppercased, and the rest of the letters of the second word / then assign result of expression to the current word
+//    if the current word does not equal a minor word,
+//        capitalize the first letter and combine with the rest of the letters, then assign the result to the current word
+//    loop through the new words array
+//        convert the first word's first letter to upper case
+//        if the current word equals 'JavaScript:',
+//            change the next word to the correct case style
+//        otherwise, if the word equals 'Programmer:',
+//            change the next word to the correct case style
+// convert the array of words into a string, assign to 'output'
+// return 'output'
